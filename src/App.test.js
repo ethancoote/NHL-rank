23e060be17ddd52ab171a2e37e2e9646d27d56d0
ethancoote <realethancoote@gmail.com>
2026-02-10 23:32:53 -0500

    Add unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,47 @@
 import { render, screen } from '@testing-library/react';
+import teamsData from './data/teamsData.json';
 import App from './App';
+import Leaderboard from './components/Leaderboard/Leaderboard';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-test('renders learn react link', () => {
+test('Leaderboard Exists', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText('Leaderboard', {selector: 'h1'});
+  expect(element).toBeInTheDocument();
 });
+
+test('Upcoming Games Exists', () => {
+  render(<App />);
+  const element = screen.getByText('Upcoming Games', {selector: 'h2'});
+  expect(element).toBeInTheDocument();
+});
+
+test('All Teams Ranked', () => {
+  render(<Leaderboard />);
+  for (let i = 0; i < teamsData.length; i++) {
+    const teamName = screen.getByText(teamsData[i].teamAbbrev);
+    expect(teamName).toBeInTheDocument();
+  }
+});
+
+test('Header Images', () => {
+  render(<Header />);
+  const logo = screen.getByAltText('nhl logo');
+  expect(logo).toBeInTheDocument();
+  expect(logo).toHaveAttribute('src', './assets/images/nhl-logo.webp');
+  const gitHubLogo = screen.getByAltText('github logo');
+  expect(gitHubLogo).toBeInTheDocument();
+  expect(gitHubLogo).toHaveAttribute('src', './assets/images/github-logo.svg');
+});
+
+test('Footer Images', () => {
+  render(<Footer />);
+  const logo = screen.getByAltText('nhl logo');
+  expect(logo).toBeInTheDocument();
+  expect(logo).toHaveAttribute('src', './assets/images/nhl-logo.webp');
+});
+
+
+
+
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+Object.defineProperty(global, 'crypto', {
+    value: {
+        randomUUID: () => Math.round(Math.random() * 10000) / 100
+    }
+});
