ca26a150f76d28134735c5b3abc557d8b3348452
ethancoote <realethancoote@gmail.com>
2026-02-13 12:55:54 -0500

    Switch to HashRouter

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router';
+import { HashRouter } from 'react-router';
 import ScrollToTop from './components/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <ScrollToTop>
       <App />
     </ScrollToTop>
-  </BrowserRouter>
+  </HashRouter>
 );
